f6dc5290dccc011a53061766833afcbb05bdaec2
DooMWhite <40442835+Doomwhite@users.noreply.github.com>
2024-09-29 13:26:52 -0300

    fix: corrige formatação (#2)

--- redis.tf
+++ redis.tf
@@ -27,18 +27,18 @@ resource "aws_elasticache_subnet_group" "redis_subnet_group" {
 }
 
 resource "aws_elasticache_replication_group" "redis_replication_group" {
-  replication_group_id          = "my-redis-replication-group"
-  description                   = "Redis Replication Group"
-  engine                        = "redis"
-  engine_version                = "7.0"
-  node_type                     = "cache.t3.micro"
-  num_cache_clusters            = 2
-  automatic_failover_enabled    = true
-  multi_az_enabled              = true
-  preferred_cache_cluster_azs   = ["sa-east-1a", "sa-east-1b"]
-  parameter_group_name          = "default.redis7"
-  subnet_group_name             = aws_elasticache_subnet_group.redis_subnet_group.id
-  security_group_ids            = [aws_security_group.redis_sg.id]
+  replication_group_id        = "my-redis-replication-group"
+  description                 = "Redis Replication Group"
+  engine                      = "redis"
+  engine_version              = "7.0"
+  node_type                   = "cache.t3.micro"
+  num_cache_clusters          = 2
+  automatic_failover_enabled  = true
+  multi_az_enabled            = true
+  preferred_cache_cluster_azs = ["sa-east-1a", "sa-east-1b"]
+  parameter_group_name        = "default.redis7"
+  subnet_group_name           = aws_elasticache_subnet_group.redis_subnet_group.id
+  security_group_ids          = [aws_security_group.redis_sg.id]
 
   tags = {
     Name = "Redis Replication Group"
